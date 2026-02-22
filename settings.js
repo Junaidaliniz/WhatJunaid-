ebb7a634399bebc7dc696b8e93e3a889ec31ccd0
Junaidaliniz <157386475+Junaidaliniz@users.noreply.github.com>
2026-02-22 08:08:06 +0500

    Create settings.js

---
+++ settings.js
@@ -0,0 +1,8 @@
+module.exports = {
+  SESSION_ID: "",  // add session Id 
+  
+  PREFIX: ".", // prefix (e.g., ., /, !, *)
+  
+  TIMEZONE: "Asia/Karachi" // put your country timeZone....leave blank if u don't know.
+};
+
