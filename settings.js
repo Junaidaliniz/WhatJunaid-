15540cac28a5903ad00d43f605415e9078e990a9
Junaidaliniz <157386475+Junaidaliniz@users.noreply.github.com>
2026-03-01 08:06:07 +0500

    Delete settings.js

--- settings.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  SESSION_ID: "",  // add session Id 
-  
-  PREFIX: ".", // prefix (e.g., ., /, !, *)
-  
-  TIMEZONE: "Asia/Karachi" // put your country timeZone....leave blank if u don't know.
-};
-
